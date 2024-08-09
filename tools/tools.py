af981508e2b7aff1bfdb6841770581b714a0b02c
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-08-09 16:00:54 -0600

    feat: add tool to get profile URL from tavily

---
+++ tools/tools.py
@@ -0,0 +1,12 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def get_profile_url_tavily(name: str):
+    """
+    Searches for LinkedIn Profile Page
+    :param name:
+    :return:
+    """
+
+    search = TavilySearchResults()
+    res = search.run(f"{name}")
+    return res[0]["url"]
