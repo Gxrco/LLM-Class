ac027ce794e06164034a55ec60cd83f68fe29d39
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-08-30 16:02:50 -0600

    feat: add agent functionality

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
-from langchain.chains.llm import LLMChain
-from langchain.prompts.prompt import PromptTemplate
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
 import os
-from third_parties.linkedin import scrape_linkedin
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
+
+load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
-    load_dotenv()
+    print("Retrieving...")
 
-    summary_template = """
-    Given the information about the person I want to create 
-    1. A short summary
-    2. Two interesting facts about him
-    """
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
 
-    summary_prompt_template = PromptTemplate(input_variable=["information"], template=summary_template)
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    result = chain.invoke(input={})
+    print(result.content)
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ.get("INDEX_NAME"), embedding=embeddings
+    )
 
-    chain = summary_prompt_template | llm
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
-    # Call function to take information from linkedin
-    linkedin_data = scrape_linkedin("https://www.linkedin.com/in/luis-von-ahn-duolingo/")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
 
-    res = chain.invoke(input={"information": linkedin_data})
+    retrieval_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(),
+        combine_docs_chain=combine_docs_chain
+    )
 
+    result = retrieval_chain.invoke({"input": query})
+
+    print(result)
+
+    template = """Use the following pieces of context to answer the question:
+    If you don't know the answer, just say you don't know, don't try to answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+
+    {context}
+
+    Question: {question}
+
+    Helpful Answer: """
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+            {"context": vectorstore.as_retriever() | format_docs, "question":
+                RunnablePassthrough()}
+            | custom_rag_prompt
+            | llm
+    )
+
+    res = rag_chain.invoke(query)
     print(res)
